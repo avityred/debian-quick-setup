7093485bede38ad489a49402b59d3bb1c3420d6e
Avity <avity@tuta.io>
2022-02-18 19:28:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo "Do you want to enable non-free repositories? (y/n)"
 read nonfree
 
-if [ nonfree = y ]
+if [ $nonfree = "y" ]
 then
     echo "Enabling non-free repos."
     sudo apt-add-repository non-free
@@ -19,7 +19,7 @@ sudo dpkg --add-architecture i386
 echo "Are there any extra apps you would like to install? (y/n)"
 read appsan
 
-if [ appsan = y ]
+if [ $appsan = "y" ]
 then
     echo "What apps do you want to install? (Must be available in the repos.)"
     read apps
@@ -33,12 +33,12 @@ fi
 echo "Do you want to also install GPU Drivers? (y/n)"
 read answer
 
-if [ answer = y ]
+if [ $answer = "y" ]
 then
     echo "Which GPU drivers do you want to install? AMD(1) or NVIDIA(2)"
     read answerg
     
-    if [ answerg = 1 ]
+    if [ $answerg = "1" ]
     then
         echo "Installing AMDGPU drivers."
         sudo apt update
@@ -47,7 +47,7 @@ then
         echo "Drivers successfully installed."
     fi
     
-    if [ answerg = 2 ]
+    if [ $answerg = "2" ]
     then
         echo "Installing NVIDIA drivers."
         sudo apt update
@@ -63,7 +63,7 @@ fi
 echo "Do you want to install some themes? (y/n)"
 read themes
 
-if [ themes = y ]
+if [ $themes = "y" ]
 then
     git clone https://github.com/alvatip/Nordzy-cursors.git && git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git && git clone https://github.com/vinceliuice/Colloid-gtk-theme.git && git clone https://github.com/vinceliuice/Colloid-icon-theme.git
     cd Nordzy-cursors
